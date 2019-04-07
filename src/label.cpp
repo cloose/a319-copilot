5676860f338c739df4def9395e4429c3bfd8870f
Christian Loose <christian.loose@hamburg.de>
2019-04-07 12:23:49 +0200

    Add label UI component

---
+++ src/label.cpp
@@ -0,0 +1,30 @@
+#include "label.h"
+
+#include <GL/gl.h>
+#include <XPLMDisplay.h>
+#include <XPLMGraphics.h>
+
+#include "utils/log.h"
+
+Label::Label(const std::string& text)
+    : m_text(text)
+{
+}
+
+Label::~Label()
+{
+}
+
+void Label::draw(int windowLeft, int windowTop, int windowRight, int windowBottom)
+{
+    static float col_white[] = {1.0, 1.0, 1.0};
+
+    int char_height;
+    XPLMGetFontDimensions(xplmFont_Proportional, NULL, &char_height, NULL);
+    int textWidth = XPLMMeasureString(xplmFont_Proportional, m_text.c_str(), m_text.length());
+
+    int left = windowLeft + (windowRight - windowLeft - textWidth) / 2;
+    int bottom = windowTop - char_height - 10;
+
+    XPLMDrawString(col_white, left, bottom, (char*)m_text.c_str(), NULL, xplmFont_Proportional);
+}
---
+++ src/label.h
@@ -0,0 +1,19 @@
+#ifndef _LABEL_H_
+#define _LABEL_H_
+
+#include <memory>
+#include <string>
+
+class Label
+{
+public:
+    explicit Label(const std::string& text);
+    virtual ~Label();
+
+    void draw(int windowLeft, int windowTop, int windowRight, int windowBottom);
+
+private:
+    std::string m_text;
+};
+
+#endif // _LABEL_H_
